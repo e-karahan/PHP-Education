913c1a46ead43c64e622c346d1b5ad4d6cadc3b5
CeyhunHouse <41891940+CeyhunHouse@users.noreply.github.com>
2020-09-30 22:50:20 +0300

    introduction-syntax

---
+++ introduction/syntax.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang=" " dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo 'Ceyhun Bahadır ÇELİK '; ?></title>
+  </head>
+  <body>
+    Bu bir HTML sayfadır.
+    <br>
+    <?php echo 'Hello World'; ?>
+    <br>
+    <?= 'Ceyhun Bahadır Çelik' ?>
+    <?php
+      // Comment Line
+      # Comment Line
+      /*
+        Comment Multi Line
+      */
+     ?>
+  </body>
+</html>
