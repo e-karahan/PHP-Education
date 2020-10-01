18a91c3667c83b2b3288388a048bd85dedfd63be
CeyhunHouse <41891940+CeyhunHouse@users.noreply.github.com>
2020-10-01 10:40:16 +0300

    variables

---
+++ 1. introduction/2.variables.php
@@ -0,0 +1,15 @@
+<?php
+  /*
+    variables;
+      - starts with $
+      - starts with character or underline '_' not number
+      - starts can by turkish characters
+      - case sensitive
+
+  */
+  $ceyhun = 'Ceyhun Bahadır Çelik';
+  $çelik = 'CEYHUNBASE';
+  $_ceyhun = 'CEYHUNBASE!';
+  $Ceyhun = 'Ceyhun Bahadır Çelik 2';
+  echo $Ceyhun ;
+?>
